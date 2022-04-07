a3fde96b43f018c2ea163b294717016d87981ea5
Biruk <biruk.rcd.1912@gmail.com>
2022-04-07 19:14:19 +0300

    django-restframework

--- Back-end/E-commerce back/store/urls.py
+++ Back-end/E-commerce back/store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list),
+    path('products/<int:id>/', views.product_detail),
 ]
--- Back-end/E-commerce back/store/views.py
+++ Back-end/E-commerce back/store/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view()
 def product_list(request):
-    return HttpResponse('ok')
+    return Response('ok')
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
