2f87e621761495b37454cc5d19dd4044a4cad60f
Bamlak Molla <94319803+BamlakMolla@users.noreply.github.com>
2022-04-21 14:13:33 +0300

    Update

---
+++ Front-end/Javascript/main.js
@@ -0,0 +1,26 @@
+var noti = document.querySelector('h4');
+	var select = document.querySelector('.select');
+	var button = document.getElementsByTagName('button');
+	for(var but of button){
+		but.addEventListener('click', (e)=>{
+			var add = Number(noti.getAttribute('data-count') || 0);
+			noti.setAttribute('data-count', add +1);
+			noti.classList.add('zero')
+
+			
+		
+
+		
+			var parent = e.target.parentNode;
+			var clone = parent.cloneNode(true);
+			select.appendChild(clone);
+			clone.lastElementChild.innerText = "Buy-now";
+			
+			if (clone) {
+				noti.onclick = ()=>{
+					select.classList.toggle('display');
+				}	
+			}
+		})
+	}
+	
